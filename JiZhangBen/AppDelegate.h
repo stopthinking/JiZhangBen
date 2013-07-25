a00b975e7e66b3f10d4e5347d33f4307442542a4
mac <mac@localhost.(none)>
2013-07-25 15:55:14 +0800

    Initial Commit

---
+++ JiZhangBen/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  JiZhangBen
+//
+//  Created by mac on 13-7-25.
+//  Copyright (c) 2013年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
